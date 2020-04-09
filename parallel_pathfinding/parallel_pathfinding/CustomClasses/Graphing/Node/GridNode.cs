12747a4f774fbe707389d77d099817847fe168c6
novyjak <novyjak@gmail.com>
2020-04-09 20:57:59 +0200

    Add function to GridNode that returns Node's coordinates

--- parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/GridNode.cs
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/GridNode.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace parallel_pathfinding.CustomClasses.Graphing.Node
+namespace parallel_pathfinding.CustomClasses.Graphing
 {
-    class GridNode
+    class GridNode : Node
     {
+        private Tuple<int, int> Position;
+        public GridNode(int PositionX, int PositionY) : base()
+        {
+            this.Position = new Tuple<int, int>(PositionX, PositionY);
+        }
+
+        public int[] GetXY()
+        {
+            int[] coordinates = [Position.Item1, Position.Item2];
+            return coordinates;
+        }
+
     }
 }
