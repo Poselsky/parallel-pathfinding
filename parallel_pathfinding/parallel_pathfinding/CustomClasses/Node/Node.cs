282668899d99a928f828b943d6943025e6023912
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-09 15:23:29 +0200

    add nodes class

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Node/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    class Node
+    {
+        public List<Node> Neighbours { get; }
+
+        public Node()
+        {
+            this.Neighbours = new List<Node>();
+        }
+
+        public void AddNeighbour(Node Neighbour)
+        {
+            Neighbours.Add(Neighbour);
+        }
+    }
+}
