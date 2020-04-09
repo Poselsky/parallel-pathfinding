5872535b943cecdafc30f377c9bf3455fb0f268f
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-09 15:04:01 +0200

    add project solution

---
+++ parallel_pathfinding/parallel_pathfinding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace parallel_pathfinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
