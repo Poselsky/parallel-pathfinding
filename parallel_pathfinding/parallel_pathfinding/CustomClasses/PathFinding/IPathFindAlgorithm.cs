2bf35336a9c0219e2aad7f1d189a60bff15b826f
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-09 15:24:14 +0200

    Create interface for pathfinding

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/PathFinding/IPathFindAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    interface IPathFindingAlgorithm
+    {
+        public double CalculateShortestPath(Node From, Node To);
+    }
+}
