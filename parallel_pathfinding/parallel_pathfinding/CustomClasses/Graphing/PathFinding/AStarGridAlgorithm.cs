e7e79ad51682a0b35996329cf382d41cde2770ec
novyjak <novyjak@gmail.com>
2020-04-09 20:52:26 +0200

    Add fuction to calculate length between two Nodes in AStarGridAlgorithm

--- parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/PathFinding/AStarGridAlgorithm.cs
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/PathFinding/AStarGridAlgorithm.cs
@@ -10,5 +10,15 @@ namespace parallel_pathfinding.CustomClasses.Graphing
         {
             throw new NotImplementedException();
         }
+
+        static private double CalculateH(GridNode From, GridNode To)
+        {
+            int[] fromxy = From.GetXY();
+            int[] toxy = To.GetXY();
+
+            double length = Math.Sqrt(Math.Pow(fromxy[0] - toxy[0], 2) + Math.Pow(fromxy[1] - toxy[1], 2)); //sqrt(a^2 + b^2) --> length of a line between two points
+
+            return length;
+        }
     }
 }
