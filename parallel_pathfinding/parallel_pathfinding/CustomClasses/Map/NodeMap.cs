c0a9410d3b887c7bdbc5fe8e9a5191fddf978b5a
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-09 15:36:39 +0200

    Redo IMap to abstract class

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Map/NodeMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    abstract class NodeMap
+    {
+        private Func<Node, Node, double> PathFind;
+        private Node[,] MatrixMapRepresentation;
+        public NodeMap (IPathFindingAlgorithm Algorithm)
+        {
+            this.PathFind = new Func<Node, Node, double>(Algorithm.CalculateShortestPath);
+        }
+
+        abstract public void GenerateMap(int Width, int Height);
+        abstract public double GetShortestPath(Node A, Node B);
+    }
+}
