48b5dbca7acfad46153b34ecdfcc1883ae9f58a5
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-09 15:23:58 +0200

    add wrapper for nodes

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Map/IMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    interface IMap
+    {
+        void GenerateMap(int Width, int Height);
+        void GetShortestPath(Node A, Node B);
+
+    }
+}
