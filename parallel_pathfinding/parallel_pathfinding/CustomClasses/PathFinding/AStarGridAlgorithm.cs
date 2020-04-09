1745a0bb580c75a208ea6a3290aeb26ee3356941
novyjak <novyjak@gmail.com>
2020-04-09 19:56:39 +0200

    Add class for A-Star Grid based algorithm

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/PathFinding/AStarGridAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    class AStarGridAlgorithm : IPathFindingAlgorithm
+    {
+        public Node[] CalculateShortestPath(Node From, Node To)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
