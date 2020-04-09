c00fc6c118dd0825d6a6b4d1476edc9b321bb737
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-09 19:01:51 +0200

    add class

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Visualising/Visualiser/ConsoleVisualiser.cs
@@ -0,0 +1,15 @@
+﻿using parallel_pathfinding.CustomClasses.Graphing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Visualising
+{
+    class ConsoleVisualiser : IVisualiser
+    {
+        public void Visualise(NodeMap Map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
