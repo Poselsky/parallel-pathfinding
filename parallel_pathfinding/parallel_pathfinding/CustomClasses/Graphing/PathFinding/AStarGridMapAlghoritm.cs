6c5ad71c1a50a11363deeef30ce5cc233c274635
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-11 19:20:12 +0200

    add empty static class for holding alghoritms

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/PathFinding/AStarGridMapAlghoritm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    public class AStarGridMapAlghoritm
+    {
+        private AStarGridMapAlghoritm() { }
+
+        public static Node[] FindShortestPathSingleThread (GridNode From, GridNode To, NodeMap Map)
+        {
+            throw new NotImplementedException("Pathfinding to be Implemented");
+        }
+
+        public static Node[] FindShortestPathInParallel(GridNode From, GridNode To, NodeMap Map)
+        {
+            throw new NotImplementedException("Pathfinding to be Implemented");
+        }
+
+        public static Node[] FindShortestPathWithSplittingThreads(GridNode From, GridNode To, NodeMap Map)
+        {
+            throw new NotImplementedException("Pathfinding to be Implemented");
+        }
+    }
+}
