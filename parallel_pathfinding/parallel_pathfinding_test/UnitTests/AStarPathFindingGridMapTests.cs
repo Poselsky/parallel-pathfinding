20be13d604cd2966e7bc2f4b816b00f1c1d4ee00
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-11 19:20:21 +0200

    add tests

--- parallel_pathfinding/parallel_pathfinding_test/UnitTests/AStarPathFindingGridMapTests.cs
+++ parallel_pathfinding/parallel_pathfinding_test/UnitTests/AStarPathFindingGridMapTests.cs
@@ -10,11 +10,34 @@ namespace parallel_pathfinding_test.UnitTests.PathFinding
     public class AStarPathFindingGridMapTests
     {
         [Test]
-        public void GetShortestPath_2By2Grid_SuccesIfIsPathFromBottomLeftAndTopRightCorner ()
+        public void GetShortestPath_2By2Grid_SuccesIfIsPathFromBottomLeftAndTopRightCorner()
         {
-            var s = new GridMap(new Func<Node, Node, NodeMap, Node[]>((a, b, c) => new Node[1]));
+            var s = new GridMap(new Func<GridNode, GridNode, NodeMap, Node[]>(AStarGridMapAlghoritm.FindShortestPathSingleThread));
 
-            s.GenerateMap(5, 5);
+            s.GenerateMap(2, 2);
+            var GeneratedShortestPath = s.GetShortestPath(s[1, 0], s[0, 1]);
+            var TrueShortestPath = new Node[] { s[1, 0], s[0, 1] };
+            for(int i = 0; i < TrueShortestPath.Length; i++)
+            {
+                if(TrueShortestPath[i] != GeneratedShortestPath[i])
+                {
+                    Assert.Fail("Shortest paths are not same. Check if order of nodes in path is correct.");
+                }
+            }
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GetShortestPath_10By1Grid_FailIfLengthOfPathIsNot10()
+        {
+            var s = new GridMap(new Func<GridNode, GridNode, NodeMap, Node[]>(AStarGridMapAlghoritm.FindShortestPathSingleThread));
+
+            s.GenerateMap(1, 10);
+
+            var GeneratedPath = s.GetShortestPath(s[0, 0], s[0, 9]);
+
+            Assert.IsTrue(GeneratedPath.Length == 10);
         }
     }
 
