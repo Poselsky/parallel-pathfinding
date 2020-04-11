cab169ecac4c6c4b8539dc23e9d34a12dfb2cb1f
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-11 12:31:16 +0200

    add position property to nodes

---
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/GridNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parallel_pathfinding.CustomClasses.Graphing
+{
+    public class GridNode : Node
+    {
+        public Tuple<int, int> Coordinates { get; private set; }
+        public GridNode(int CoordinateX, int CoordinateY)
+        {
+            Coordinates = new Tuple<int, int>(CoordinateX, CoordinateY);
+        }
+    }
+}
