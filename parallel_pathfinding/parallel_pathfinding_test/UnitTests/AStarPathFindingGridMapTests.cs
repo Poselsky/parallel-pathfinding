7f8dc4b819d09ca678c02e11ed38a3e71e035984
Oleg Musijenko <oleg.musijenko@seznam.cz>
2020-04-11 18:55:32 +0200

    rename filename to better match test method, remove inner class

---
+++ parallel_pathfinding/parallel_pathfinding_test/UnitTests/AStarPathFindingGridMapTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using parallel_pathfinding.CustomClasses.Graphing;
+
+namespace parallel_pathfinding_test.UnitTests.PathFinding
+{
+    [TestFixture]
+    public class AStarPathFindingGridMapTests
+    {
+        [Test]
+        public void GetShortestPath_2By2Grid_SuccesIfIsPathFromBottomLeftAndTopRightCorner ()
+        {
+            var s = new GridMap(new Func<Node, Node, NodeMap, Node[]>((a, b, c) => new Node[1]));
+
+            s.GenerateMap(5, 5);
+        }
+    }
+
+}
