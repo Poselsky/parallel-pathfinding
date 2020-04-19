e83fb3d8df4d15b21250b1925ba8020b47a2289c
novyjak <novyjak@gmail.com>
2020-04-19 21:48:19 +0200

    Convert AGridNode's properties to Node

--- parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/Node.cs
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/Node.cs
@@ -7,7 +7,8 @@ namespace parallel_pathfinding.CustomClasses.Graphing
     public class Node
     {
         public List<Tuple<Node, double>> Neighbours { get; }
-        protected double gCost; //current lenght from starting Node, starting Node has g = 0
+        private double _gCost; //current lenght from starting Node, starting Node has g = 0
+        private double _hCost;
 
         public Node()
         {
@@ -19,9 +20,22 @@ namespace parallel_pathfinding.CustomClasses.Graphing
             Neighbours.Add(new Tuple<Node, double>(Neighbour, Distance));
         }
 
+        public void SetGCost(double gCost)
+        {
+            _gCost = gCost;
+        }
+
+        public void SetHCost(double hCost)
+        { _hCost = hCost; }
+
         public double GetGCost()
         {
-            return gCost;
+            return _gCost;
         }
+
+        public double GetHCost()
+        { return _hCost; }
+        public double GetFCost()
+        { return _hCost + _gCost; }
     }
 }
