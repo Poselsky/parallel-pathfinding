cd97cd4d58945a2d1b58be35d5ff4e940a63f843
novyjak <novyjak@gmail.com>
2020-04-19 12:52:33 +0200

    Add private hCost and fCost variables with Get functions.

--- parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/AGridNode.cs
+++ parallel_pathfinding/parallel_pathfinding/CustomClasses/Graphing/Node/AGridNode.cs
@@ -2,9 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace parallel_pathfinding.CustomClasses.Graphing.Node
+namespace parallel_pathfinding.CustomClasses.Graphing
 {
-    class AGridNode
+    class AGridNode : GridNode
     {
+        private double hCost;
+        private double fCost;
+
+        public AGridNode(int PositionX, int PositionY) : base(PositionX, PositionY)
+        {}
+
+        public void SetGHCost(double gCost, double hCost)
+        {
+            this.gCost = gCost;
+            this.hCost = hCost;
+            this.fCost = this.gCost + this.hCost;
+        }
+
+        public double GetHCost()
+        { return hCost; }
+        public double GetFCost()
+        { return fCost; }
     }
 }
